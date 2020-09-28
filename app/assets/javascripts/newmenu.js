206e4c37e9fe34cc02349df73f8c0d340a7bdda5
Max Froumentin <github@lapin-bl.eu>
2020-09-28 17:30:09 +0100

    operational menu with sub-items on mobile

--- app/assets/javascripts/newmenu.js
+++ app/assets/javascripts/newmenu.js
@@ -1,31 +1,10 @@
-/**
-* Toggle class
-* @param {object} node element
-* @param {string} className to toggle
-*/
-const toggleClass = function (node, className) {
-    if (node.className.indexOf(className) > 0) {
-      node.className = node.className.replace(' ' + className, '');
-    } else {
-      node.className += ' ' + className;
-    }
-  };
-  
 
-const headerButton = document.querySelector('button.govuk-header__menu-button');
-
-
-
-headerButton.addEventListener('click', event => {
-    var $toggleButton = event.target || event.srcElement;
-    var $target = event.target; //$module.querySelector('#' + $toggleButton.getAttribute('aria-controls'));
-  
-    toggleClass($target, 'govuk-header__navigation--open');
-    toggleClass($toggleButton, 'govuk-header__menu-button--open');
-    $toggleButton.innerText = 
-      $toggleButton.classList.contains('govuk-header__menu-button--open') ? 'Close' : 'Menu';
-    $toggleButton.setAttribute('aria-expanded', $toggleButton.getAttribute('aria-expanded') !== 'true');
-    $target.setAttribute('aria-hidden', $target.getAttribute('aria-hidden') === 'false');
+$('.xpl-menu__button').on('click', event => {
+  $('.xpl-menu__button').hide();
+  $(event.target).next().show();
 });
 
-  
+$('.xpl-menu__button-back').on('click', event => {
+  $('.xpl-menu__frame2').hide();
+  $('.xpl-menu__button').show();
+});
