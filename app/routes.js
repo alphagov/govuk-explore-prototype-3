65b65570df3fe03be7f6bb87d99e995f9705a610
Max Froumentin <github@lapin-bl.eu>
2020-10-14 15:26:01 +0100

    Get all activities, and for subtopics as well

--- app/routes.js
+++ app/routes.js
@@ -6,41 +6,20 @@ const url = require('url');
 // Add your routes here - above the module.exports line
 
 const BASE_URL = 'https://govuk-explore-api-prototype.herokuapp.com/'
-//const BASE_URL = 'http://localhost:3000/'
+//const BASE_URL = 'http://localhost:3050/'
 
 router.get('/browse/:topicSlug', function (req, res) {
   topicSlug = req.params.topicSlug
 
   request(BASE_URL + 'browse/' + topicSlug, { json: true }, (error, result, body) => {
     body.topicSlug = topicSlug;
-
     body.organisations = body.organisations.slice(0,5);
-    console.log(JSON.stringify(body.organisations, null, 2));
-
-    body.activities = [
-      {
-        title: "Statement of changes to the Immigration Rules: HC 707, 10 September 2020",
-        url: "/",
-        type: "Speech",
-        category: "Policy papers and consultation",
-        published: "12 August 2020"
-      },
-      {
-        title: "Criminal investigations: use of interpreters",
-        url: "/",
-        type: "Guidance",
-        category: "Guidance and regulation",
-        published: "12 August 2020"
-      },
-      {
-        title: "Home Secretary annouces details of the Hong Kong BN(O) Visa",
-        url: "/",
-        type: "Speech",
-        category: "News and communications",
-        published: "12 August 2020"
-      }
-    ];
-    console.log('requesting', BASE_URL + 'browse/' + topicSlug);
+    body.latest_news.forEach(news => {
+      const d = new Date(news.public_timestamp);
+      news.date = `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
+      news.subtopic = news.subtopic === 'other' ? '' : news.subtopic.replace(/_/g, ' ');
+      news.topic = news.topic === 'other' ? '' : news.topic.replace(/_/g, ' ');
+    });
     res.render('topic', body)
   })
 })
@@ -50,9 +29,18 @@ router.get('/browse/:topicSlug/:subTopicSlug', function (req, res) {
   topicSlug = req.params.topicSlug
   subTopicSlug = req.params.subTopicSlug
 
-  request(BASE_URL + 'browse/' + topicSlug + '/' + subTopicSlug, { json: true }, (error, result, bodySubtopic) => {
+  request(BASE_URL + 'browse/' + topicSlug + '/' + subTopicSlug, { json: true }, (error, result, body) => {
     request(BASE_URL + 'browse/' + topicSlug, { json: true }, (error, result, bodyTopic) => {
-      res.render('sub_topic', { ...bodySubtopic, parent: bodyTopic.title });
+      body.topicSlug = topicSlug;
+      body.organisations = body.organisations.slice(0,5);
+      body.latest_news.forEach(news => {
+        const d = new Date(news.public_timestamp);
+        news.date = `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
+        news.subtopic = news.subtopic === 'other' ? '' : news.subtopic.replace(/_/g, ' ');
+        news.topic = news.topic === 'other' ? '' : news.topic.replace(/_/g, ' ');
+      });
+
+      res.render('sub_topic', { ...body, parent: bodyTopic.title });
     });
   });
 });
