7cc5812e64c0576715e72a63b6505ff10497b741
Max Froumentin <github@lapin-bl.eu>
2020-10-30 12:14:52 +0000

    Forgot to change to the production API. Again.

--- app/routes.js
+++ app/routes.js
@@ -6,8 +6,8 @@ const request = require('request');
 const url = require('url');
 // Add your routes here - above the module.exports line
 
-//const BASE_URL = 'https://govuk-explore-api-prototype.herokuapp.com/'
-const BASE_URL = 'http://localhost:3050/'
+const BASE_URL = 'https://govuk-explore-api-prototype.herokuapp.com/'
+//const BASE_URL = 'http://localhost:3050/'
 
 
 router.get('/browse/:topicSlug', function (req, res) {
