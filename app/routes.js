e2761d0e01005037a32b854a7b5915b74e2ab34c
Max Froumentin <github@lapin-bl.eu>
2020-11-02 16:21:16 +0000

    Make links relative to avoid user moving away from this prototype

--- app/routes.js
+++ app/routes.js
@@ -80,7 +80,8 @@ router.get('/', function (req, res) {
       const newBody = body
         .replace(/(href|src)="\//g, '$1="https://www.gov.uk/')
         .replace(/<header[^]+<\/header>/, headerStringWithCss)
-        .replace(/<\/body>/,'<script src="/public/javascripts/newmenu.js"></script>\n</body>');
+        .replace(/<\/body>/,'<script src="/public/javascripts/newmenu.js"></script>\n</body>')
+        .replace(/<a(.*) href\s*=\s*(['"])\s*(https:)?\/\/www.gov.uk\//g,'<a $1 href=$2/');
 
       res.send(newBody);
 
